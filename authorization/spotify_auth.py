40dee73160a3b739016ec745e469e48fb2a4f859
RUBY\carissa.thomas <cthomas@callruby.com>
2018-11-20 11:26:45 -0800

    flask implemented

--- authorization/spotify_auth.py
+++ authorization/spotify_auth.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     username = input("Enter in your spotify username: ")
 
     loginPrompt = spotipy.util.prompt_for_user_token(
-        username, scope=None, client_id=credentials.client_id, client_secret=credentials.client_secret, redirect_uri="http://spotifygenie.com")
+        username, scope=None, client_id=credentials.client_id, client_secret=credentials.client_secret, redirect_uri="http://localhost:5000/")
 
 # set up a user log in
